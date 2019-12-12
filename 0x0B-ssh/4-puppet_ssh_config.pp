bf314ca40bcda8b644bda4b02b271794b3690f60
sranciato <sranciato1@gmail.com>
2019-12-12 18:34:33 +0000

    [ADD] puppet file fixed

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,9 +1,12 @@
 # change config file lines
-file_line {
+file_line { swap password auth':
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
 }
-file_line {
+
+file_line { 'swap identity':
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/holberton',
 }
