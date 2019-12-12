bef4d84090d51bbeba6944910547093e614dbc34
sranciato <sranciato1@gmail.com>
2019-12-12 18:10:26 +0000

    [ADD] puppet file fixed

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,11 +1,9 @@
 # change config file lines
 file_line {
-  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
 }
 file_line {
-  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/holberton',
 }
