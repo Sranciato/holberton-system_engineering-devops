6ae76a6a449686eb59b0047c133d22a3ba2de63c
sranciato <sranciato1@gmail.com>
2019-12-12 17:56:41 +0000

    [ADD] puppet advanced

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -2,11 +2,12 @@
 
 file_line {
   ensure => present,
-  path   => '/etc/ssh/ssh_config'
-  line   => '\tPasswordAuthentication no'
+  path   => '/etc/ssh/ssh_config',
+  line   => '\tPasswordAuthentication no',
+  match  => 'PasswordAuthentication',
 }
 file_line {
   ensure => present,
-  path => '/etc/ssh/ssh_config'
-  line => '\tIdentityFile ~/.ssh/holberton'
+  path   => '/etc/ssh/ssh_config',
+  line   => '\tIdentityFile ~/.ssh/holberton',
 }
