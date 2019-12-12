81cfe19a11eeea22b5f89c7053ad06d82909589a
sranciato <sranciato1@gmail.com>
2019-12-12 18:03:09 +0000

    [ADD] chmod

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # change config file lines
 
 file_line {
-  ensure => present,
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '\tPasswordAuthentication no',
   match  => 'PasswordAuthentication',
 }
 file_line {
-  ensure => present,
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '\tIdentityFile ~/.ssh/holberton',
 }
