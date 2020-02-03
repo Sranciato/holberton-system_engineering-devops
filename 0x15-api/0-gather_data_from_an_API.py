3eb99f8774196c6778488d8a2e6dc9205cb80f64
sranciato <sranciato1@gmail.com>
2020-02-03 17:08:09 +0000

    [ADD] task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    total_tasks = 0
+    completed_tasks = 0
+    task_list = []
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(argv[1])).json()
+    url = "https://jsonplaceholder.typicode.com/todos"
+    for task in requests.get(url).json():
+        if user["id"] == task["userId"]:
+            total_tasks += 1
+            if task["completed"] is True:
+                task_list.append(task["title"])
+                completed_tasks += 1
+    print("Employee {} is done with tasks({}/{})".format(
+        user["name"], completed_tasks, total_tasks))
+    for title in task_list:
+        print("\t{}".format(title))
