130d2c71645400e0457c3ec4bc86fdc13761dd5c
sranciato <sranciato1@gmail.com>
2020-02-03 21:42:35 +0000

    [FIX] write different tasks

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -4,13 +4,13 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    task_dict = {}
     new_list = []
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
                         .format(argv[1])).json()
     url = "https://jsonplaceholder.typicode.com/todos"
     for task in requests.get(url).json():
         if user["id"] == task["userId"]:
+            task_dict = {}
             task_dict["task"] = task["title"]
             task_dict["completed"] = task["completed"]
             task_dict["username"] = user["username"]
