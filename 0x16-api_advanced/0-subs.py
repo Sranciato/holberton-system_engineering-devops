777a44c5df15e828ed36e91707b1e353e5b6a2bd
sranciato <sranciato1@gmail.com>
2020-02-06 19:26:24 +0000

    [ADD] task 1

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    """Get the total number of subscribers from a subreddit"""
+    about = requests.get(
+        'https://api.reddit.com/r/{}/about'.format(subreddit),
+        headers={'User-Agent': 'FancyRancy'}
+    ).json()
+    if ('modhash' in about['data']):
+        return(0)
+    return(about['data']['subscribers'])
