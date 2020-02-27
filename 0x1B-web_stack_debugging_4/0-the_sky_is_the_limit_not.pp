6ed89a934b1d58a043e9b1c778ffe1cdf8cdebf8
sranciato <sranciato1@gmail.com>
2020-02-27 23:28:34 +0000

    [ADD] works on container... but not for the checker

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 #Change the ulimit that causes too many open files
 exec { 'handle ulimit':
   path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
-  command => "sudo sed -i 's/15/10000/g' /etc/default/nginx"; sudo service nginx restart,
+  command => "sudo sed -i 's/15/10000/g' /etc/default/nginx; sudo service nginx restart",
 }
