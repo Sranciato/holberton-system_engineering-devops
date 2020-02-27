5c0c00d993cf0563defdbfcb314fbc68e5d7344e
sranciato <sranciato1@gmail.com>
2020-02-27 22:37:14 +0000

    [ADD] make ulimit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,9 @@
 #Change the ulimit that causes too many open files
 exec { 'handle ulimit':
-  command => sudo /bin/sed -i 's/15/10000/' /etc/default/nginx; sudo /usr/sbin/service nginx restart
+     command => 'sudo sed -i \'s/15/10000/\' /etc/default/nginx',
+     path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+exec { 'restart nginx':
+     command => 'sudo service nginx restart',
+     path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
