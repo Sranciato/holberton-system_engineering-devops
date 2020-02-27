cf2e49caeb8255a5fe2acdc0a073e871be47645d
sranciato <sranciato1@gmail.com>
2020-02-27 22:16:18 +0000

    [ADD] sudo

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 #Change the ulimit that causes too many open files
 exec { 'handle ulimit':
-  command => '/bin/sed -i \'s/15/1000/\' /etc/default/nginx'
+  command => 'sudo /bin/sed -i \'s/15/1000/\' /etc/default/nginx'
 }
 exec { 'nginx restart':
-  command => '/usr/bin/service nginx restart'
+  command => 'sudo /usr/bin/service nginx restart'
 }
