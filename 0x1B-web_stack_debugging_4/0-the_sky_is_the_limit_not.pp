9d517cf23390b9ae1bdad8069a318aa55f35f415
sranciato <sranciato1@gmail.com>
2020-02-27 23:11:46 +0000

    [ADD] put paths in a list format

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,10 @@
 #Change the ulimit that causes too many open files
 exec { 'handle ulimit':
+  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
   command => 'sudo sed -i \'s/15/10000/g\' /etc/default/nginx',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
 exec { 'restart nginx':
+  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
   command => 'sudo service nginx restart',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
